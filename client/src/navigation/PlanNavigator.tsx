17037498d4fd00efb2c4acee6f091ba5d24f757f
dehelean-catalin <drcatalin00@gmail.com>
2026-08-22 16:25:26 +0300

    Update libraries

--- client/src/navigation/PlanNavigator.tsx
+++ client/src/navigation/PlanNavigator.tsx
@@ -6,48 +6,48 @@ import { WorkoutPlansScreen } from "@/screens/Plans/PlansScreen";
 import { WorkoutPlanForm } from "@/screens/WorkoutPlanForm/WorkoutPlanForm";
 import WorkoutPlanPreview from "@/screens/WorkoutPlanPreview/WorkoutPlanPreview";
 import WorkoutPreview from "@/screens/WorkoutPreview/WorkoutPreview";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function PlanNavigator() {
-	const options = { headerTitle: "" };
+  const options = { headerTitle: "" };
 
-	return (
-		<Stack.Navigator>
-			<Stack.Screen
-				name={routes.PLAN}
-				component={WorkoutPlansScreen}
-				options={{ headerTitle: "Plans" }}
-			/>
-			<Stack.Screen
-				name={routes.CREATE_PLAN}
-				component={WorkoutPlanForm}
-				options={{
-					headerTitle: "Create plan",
-				}}
-			/>
-			<Stack.Screen name={routes.EXERCISE} component={ExercisesScreen} />
-			<Stack.Screen
-				name={routes.EXERCISE_DETAILS}
-				component={ExerciseDetailsScreen}
-				options={options}
-			/>
-			<Stack.Screen
-				name={routes.CREATE_EXERCISE}
-				component={CreateExerciseScreen}
-			/>
-			<Stack.Screen
-				name={routes.WORKOUT_PREVIEW}
-				component={WorkoutPlanPreview}
-				options={options}
-			/>
-			<Stack.Screen
-				name={routes.WORKOUT_DAY_PREVIEW}
-				component={WorkoutPreview}
-				options={options}
-			/>
-		</Stack.Navigator>
-	);
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={routes.PLAN}
+        component={WorkoutPlansScreen}
+        options={{ headerTitle: "Plans" }}
+      />
+      <Stack.Screen
+        name={routes.CREATE_PLAN}
+        component={WorkoutPlanForm}
+        options={{
+          headerTitle: "Create plan",
+        }}
+      />
+      <Stack.Screen name={routes.EXERCISE} component={ExercisesScreen} />
+      <Stack.Screen
+        name={routes.EXERCISE_DETAILS}
+        component={ExerciseDetailsScreen}
+        options={options}
+      />
+      <Stack.Screen
+        name={routes.CREATE_EXERCISE}
+        component={CreateExerciseScreen}
+      />
+      <Stack.Screen
+        name={routes.WORKOUT_PREVIEW}
+        component={WorkoutPlanPreview}
+        options={options}
+      />
+      <Stack.Screen
+        name={routes.WORKOUT_DAY_PREVIEW}
+        component={WorkoutPreview}
+        options={options}
+      />
+    </Stack.Navigator>
+  );
 }
